39ae5d714807ea46d4d2215cf02889a603a2e1ed
Lisofffa <lisofffa@gmail.com>
2024-04-16 13:34:53 +0300

    fix: added default image

--- src/contentscript/multitable-panel/components/image.tsx
+++ src/contentscript/multitable-panel/components/image.tsx
@@ -1,20 +1,36 @@
 import React, { FC, useState } from 'react'
 
-export interface Props {
-  image?: {
+export interface ImageProps {
+  cid: {
     ipfs_cid?: string
     url?: string
   }
+}
+
+export interface Props {
+  image?:
+    | {
+        ipfs_cid?: string
+        url?: string
+      }
+    | string
 
   fallbackUrl?: string
   alt?: string
 }
 
 export const Image: FC<Props> = ({ image, alt, fallbackUrl }) => {
-  const [imageUrl, setImageUrl] = useState(
-    (image?.ipfs_cid ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}` : image?.url) ||
-      fallbackUrl
-  )
+  const [imageUrl, setImageUrl] = useState(() => {
+    if (typeof image === 'string') {
+      return image
+    } else if (image) {
+      return image.ipfs_cid
+        ? `https://ipfs.near.social/ipfs/${image.ipfs_cid}`
+        : image.url || fallbackUrl
+    } else {
+      return fallbackUrl || ''
+    }
+  })
 
   return (
     <img
